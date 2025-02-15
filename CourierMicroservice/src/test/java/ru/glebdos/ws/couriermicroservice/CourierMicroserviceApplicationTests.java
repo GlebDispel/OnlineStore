581eee554b85ec1540353f186454feeb5d3c0bde
Chalov Gleb <petrkratkij@gmail.com>
2025-02-15 20:09:17 +0300

    test again

---
+++ CourierMicroservice/src/test/java/ru/glebdos/ws/couriermicroservice/CourierMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glebdos.ws.couriermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ OrderMicroservice/src/test/java/ru/glebdos/ws/orderservice/OrderServiceImplApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glebdos.ws.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
